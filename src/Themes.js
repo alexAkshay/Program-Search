20ef1254d3df3adc95f80b05988895674b411680
alexAkshay <93480740+alexAkshay@users.noreply.github.com>
2022-07-05 12:08:02 +0530

    new Fix

--- src/Themes.js
+++ src/Themes.js
@@ -3,12 +3,11 @@ import React  from "react";
 export const themes = {
     dark :{
         color: "white",
-        backgroundImage: "url('https://i.pinimg.com/originals/67/82/8e/67828e156f4f7e9aa72fb3030f0c7493.jpg')"
+        backgroundColor: "#164569"
     },
     light :{
         color: "black",
-        backgroundImage: "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0p9fIx1Ve9GNawPxenG_JBQlhRJAUICuUcwxedICmr5AH_jZWI9IUu7w1Yay2kyem9DY&usqp=CAU')"
-    }
+        backgroundColor:  "#8DCFEC" }
 }
 
 const ThemesContext = React.createContext(themes.dark);
